e4feabd014081d002531c0e7d2970bf951e85e5a
Erik Tech <eriksantos0126@gmail.com>
2026-02-19 23:48:47 -0300

    Reorder model creation and router inclusion

--- app/main.py
+++ app/main.py
@@ -3,9 +3,8 @@ from .database import engine
 from . import models
 from .routers import animes, auth
 
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(animes.router)
+  models.Base.metadata.create_all(bind=engine)
